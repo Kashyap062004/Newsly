6a7e9a5300d26c5f444ea93aed431b65b09a514d
Kashyap062004 <kashyap.trivedi2004@gmail.com>
2025-07-11 19:14:52 +0530

    deployment

--- server/index.js
+++ server/index.js
@@ -35,7 +35,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 // app.use(express.json());
 app.use('/api/payment/webhook', express.raw({ type: 'application/json' }));
